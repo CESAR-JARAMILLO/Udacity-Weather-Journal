d37528fc930b6b228a980c1321c07de64f329d82
Cesar Jaramillo <csarjaramillo89@yahoo.com>
2021-02-05 15:31:13 -0800

    feat: Initialize post route

--- server.js
+++ server.js
@@ -38,3 +38,12 @@ function callback(req, res) {
 }
 
 // Post Route
+app.post('/add', addData);
+
+function addData(req, res) {
+    projectData['temp'] = req.body.temp;
+    projectData['date'] = req.body.date;
+    projectData['content'] = req.body.content;
+
+    res.send(projectData);
+}
