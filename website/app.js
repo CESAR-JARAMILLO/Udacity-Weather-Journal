8080b4221e5e0c8ae153dc33105690388b56f584
Cesar Jaramillo <csarjaramillo89@yahoo.com>
2021-02-08 11:01:30 -0800

    feat: POST data

--- website/app.js
+++ website/app.js
@@ -30,6 +30,24 @@ const getWeather = async (baseUrl, zip, key) => {
 }
 
 /* Function to POST data */
+const postData = async (url = '', data = {}) => {
+
+    const response = await fetch(url, {
+        method: 'POST',
+        credentials: 'same-origin',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(data)
+    });
+
+    try {
+        const newData = await response.json();
+        return newData;
+    } catch(error) {
+        console.log('error', error)
+    }
+};
 
 
 /* Function to GET Project Data */
